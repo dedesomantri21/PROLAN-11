7b04feab298a5c8be0a1a61f338a553e42081195
dedesomantri21 <dedesomantri44@gmail.com>
2017-02-28 21:30:38 +0700

    Add files via upload

---
+++ Tugas1/HelloWorld.java
@@ -0,0 +1,11 @@
+//PROGRAM 
+/*
+    
+    "HelloWorld.java".
+*/
+public class HelloWorld {
+  // main()
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
