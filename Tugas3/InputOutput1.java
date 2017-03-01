f750ade7cf15abbf37d01c750f6ee28744a6cfb4
Dede Somatri <dedesomantri44@gmail.com>
2017-03-01 20:36:46 +0700

    Add files via upload

---
+++ Tugas3/InputOutput1.java
@@ -0,0 +1,10 @@
+public class InputOutput1{
+	public static void main (String [] argv) {
+		System.out.print("Belajar Java Yuks");
+		System.out.println("Belajar Java Yuks1");
+		System.out.println("Belajar Java Yuks2");
+		System.out.print("Belajar Java Yuks3");
+		//System.out.println ==> pakai ENTER
+		//System.out.print==> tidak pakai ENTER
+	}
+}
---
+++ Tugas3/InputOutput2.java
@@ -0,0 +1,6 @@
+public class InputOutput2{
+	public static void main (String [] argv) {
+		System.out.print ("Balajar \nJava \nYuks");
+		//n berfungsi untuk ENTER tanpa membuat System.out.print/println berkali-kali
+	}
+}
