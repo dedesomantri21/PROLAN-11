e74f1e9cc417b7e232506e42f360d5d1938a91a2
Dede Somatri <dedesomantri44@gmail.com>
2017-03-21 22:13:53 +0700

    Add files via upload

---
+++ Tugas7/alatmusik.java
@@ -0,0 +1,14 @@
+public class alatmusik {
+  protected String namaalat;
+  protected String jenismsk;
+   protected Integer harga;
+  
+
+  public void Informasimusik() {
+	
+    System.out.print("NAMA ALAT MUSIK          : " + namaalat + "\n");
+    System.out.print("Jenis    : " + jenismsk + " \n");
+    System.out.print("Harga    : " + harga + "\n");
+    System.out.print("\n");
+  }
+}
